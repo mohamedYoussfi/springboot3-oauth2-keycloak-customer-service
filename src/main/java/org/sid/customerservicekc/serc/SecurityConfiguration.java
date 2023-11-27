06cb586df03d0715775d8982ba6ee0e96e76e52a
Mohamed Youssfi <med@youssfi.net>
2023-11-27 20:47:44 +0100

    SC

--- src/main/java/org/sid/customerservicekc/serc/SecurityConfiguration.java
+++ src/main/java/org/sid/customerservicekc/serc/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package org.sid.customerservicekc.serc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,13 +13,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+    private final JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfiguration(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         http.csrf(csrf->csrf.disable());
         http.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
-        http.oauth2ResourceServer(ors -> ors.jwt(Customizer.withDefaults()));
+        http.oauth2ResourceServer(ors -> {
+            ors.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter));
+        });
         http.sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
--- src/main/java/org/sid/customerservicekc/web/CustomerRestController.java
+++ src/main/java/org/sid/customerservicekc/web/CustomerRestController.java
@@ -3,9 +3,12 @@ package org.sid.customerservicekc.web;
 import org.sid.customerservicekc.entities.Customer;
 import org.sid.customerservicekc.repository.CustomerRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 import java.util.List;
 @RestController
 public class CustomerRestController {
@@ -15,13 +18,17 @@ public class CustomerRestController {
         this.customerRepository = customerRepository;
     }
     @GetMapping("/customers")
-    //@PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('USER')")
     public List<Customer> customerList(){
         return customerRepository.findAll();
     }
     @GetMapping("/customers/{id}")
-    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public Customer customerById(@PathVariable Long id){
         return customerRepository.findById(id).get();
+    }
+    @GetMapping("/auth")
+    public Principal authentication(Principal principal){
+        return principal;
     }
 }
